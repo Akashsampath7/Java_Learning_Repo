4bc665ac871b7abd64008b4762174582a74e61fb
Akashsampath7 <132433640+Akashsampath7@users.noreply.github.com>
2025-06-03 13:54:29 +0530

    Add files via upload

---
+++ first_java/new_learning_method/Car.java
@@ -0,0 +1,14 @@
+package new_learning_method;
+
+import first_java.NewMethodFile;
+
+public class Car {
+
+	public static void main(String[] args) {
+		int Num = NewMethodFile.addingNumber(30);
+			System.out.println("adding numbers: "+ Num);
+			
+		NewMethodFile myNum = new NewMethodFile();
+		myNum.adding2Numbers(200);
+	}
+}
